ab3eb9256728a5cb47c7d56513825ef87dd2f1b0
Dipanjan2910 <dipanjan860@gmail.com>
2023-12-20 19:34:09 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,204 @@
+const generatePalette = () => {
+    for (let i = 1; i <= 24; i++) {
+      let randomHex = Math.floor(Math.random() * 0xffffff).toString(16);
+      randomHex = `#${randomHex.padStart(6, "0")}`;
+      const body = document.getElementById(`${i}`);
+      const hexCode = document.getElementById(`code-${i}`);
+      body.style.backgroundColor = randomHex;
+      hexCode.innerText = randomHex;
+    }
+  }
+  
+  const copy1 = () => {
+    const code = document.getElementById("code-1");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy2 = () => {
+    const code = document.getElementById("code-2");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy3 = () => {
+    const code = document.getElementById("code-3");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy4 = () => {
+    const code = document.getElementById("code-4");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy5 = () => {
+    const code = document.getElementById("code-5");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy6 = () => {
+    const code = document.getElementById("code-6");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy7 = () => {
+    const code = document.getElementById("code-7");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy8 = () => {
+    const code = document.getElementById("code-8");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy9 = () => {
+    const code = document.getElementById("code-9");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy10 = () => {
+    const code = document.getElementById("code-10");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy11 = () => {
+    const code = document.getElementById("code-11");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy12 = () => {
+    const code = document.getElementById("code-12");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy13 = () => {
+    const code = document.getElementById("code-13");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy14 = () => {
+    const code = document.getElementById("code-14");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy15 = () => {
+    const code = document.getElementById("code-15");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy16 = () => {
+    const code = document.getElementById("code-16");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy17 = () => {
+    const code = document.getElementById("code-17");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy18 = () => {
+    const code = document.getElementById("code-18");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy19 = () => {
+    const code = document.getElementById("code-19");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy20 = () => {
+    const code = document.getElementById("code-20");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy21 = () => {
+    const code = document.getElementById("code-21");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy22 = () => {
+    const code = document.getElementById("code-22");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy23 = () => {
+    const code = document.getElementById("code-23");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  const copy24 = () => {
+    const code = document.getElementById("code-24");
+    console.log(code.innerText);
+    navigator.clipboard.writeText(code.innerText);
+    const copy = code.innerText;
+    code.innerText = "Copied";
+    setTimeout(() => code.innerText = copy, 1000);
+  }
+  
